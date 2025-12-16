89f06168d8bf64898c7fdf48ce9b307beaa46db1
XERYVEL <julianfer.ar@gmail.com>
2025-12-16 11:20:12 -0300

    Hay un problema al iniciar el juego

--- src/main/java/main/AssetSetter.java
+++ src/main/java/main/AssetSetter.java
@@ -19,27 +19,8 @@ public class AssetSetter {
     public void setNPC() {
 
         int mapNum = 0;
-        gp.npc[mapNum][0] = new NPC_Panadera(gp);
-        gp.npc[mapNum][0].worldX = 13 * gp.tileSize;
-        gp.npc[mapNum][0].worldY = 5 * gp.tileSize;
-        gp.npc[mapNum][0].mapIndex = mapNum;
-        if(!gp.videoMostrado){
-            gp.npc[mapNum][1] = new NPC_Pavon(gp);
-            gp.npc[mapNum][1].worldX = 44 * gp.tileSize;
-            gp.npc[mapNum][1].worldY = 7 * gp.tileSize;
-            gp.npc[mapNum][1].mapIndex = mapNum;
-        }
 
         mapNum = 1;
-        gp.npc[mapNum][2] = new NPC_Aila(gp);
-        gp.npc[mapNum][2].worldX = 18 * gp.tileSize;
-        gp.npc[mapNum][2].worldY = 23 * gp.tileSize;
-        gp.npc[mapNum][2].mapIndex = mapNum;
-
         mapNum = 2;
-        gp.npc[mapNum][3] = new NPC_LupeMarciana(gp);
-        gp.npc[mapNum][3].worldX = 45 * gp.tileSize;
-        gp.npc[mapNum][3].worldY = 5 * gp.tileSize;
-        gp.npc[mapNum][3].mapIndex = mapNum;
         }
 }
--- src/main/java/main/KeyHandler.java
+++ src/main/java/main/KeyHandler.java
@@ -6,13 +6,26 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener{
 
     gamePanel gp;
-    public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed;
 
-    boolean checkDrawTime = false;
+    // Player 1 (WASD)
+    public boolean upPressed = false;
+    public boolean downPressed = false;
+    public boolean leftPressed = false;
+    public boolean rightPressed = false;
+
+    // Player 2 (Flechas)
+    public boolean arrowUpPressed = false;
+    public boolean arrowDownPressed = false;
+    public boolean arrowLeftPressed = false;
+    public boolean arrowRightPressed = false;
+
+    public boolean enterPressed = false;
+    public boolean checkDrawTime = false;
 
     public KeyHandler(gamePanel gp) {
-        this.gp =gp;
+        this.gp = gp;
     }
+
     @Override
     public void keyTyped(KeyEvent e) {
     }
@@ -44,76 +57,44 @@ public class KeyHandler implements KeyListener{
 
     public void titleState(int code) {
         if(gp.ui.titleScreenState == 0){
-            if(code == KeyEvent.VK_W) {
+            if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
                 gp.ui.commandNum--;
                 if(gp.ui.commandNum < 0) {
                     gp.ui.commandNum = 1;
                 }
+                gp.playSE(5);
             }
-            if(code == KeyEvent.VK_S) {
+            if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
                 gp.ui.commandNum++;
                 if(gp.ui.commandNum > 1) {
                     gp.ui.commandNum = 0;
                 }
+                gp.playSE(5);
             }
             if(code == KeyEvent.VK_ENTER) {
                 if(gp.ui.commandNum == 0) {
-                    gp.reloj.reiniciarTiempo();
+                    // Iniciar juego
+                    if(gp.reloj != null) {
+                        gp.reloj.reiniciarTiempo();
+                    }
                     gp.gameState = gp.playState;
-
+                    gp.playMusic(7);
                 }
                 if(gp.ui.commandNum == 1) {
+                    // Salir
                     System.exit(0);
                 }
             }
-            else if(gp.gameState == gp.playState){
-                if(code == KeyEvent.VK_W) {
-                    gp.ui.commandNum--;
-                    if(gp.ui.commandNum < 0) {
-                        gp.ui.commandNum = 3;
-                    }
-                }
-                if(code == KeyEvent.VK_S) {
-                    gp.ui.commandNum++;
-                    if(gp.ui.commandNum > 3) {
-                        gp.ui.commandNum = 0;
-                    }
-                }
-                if(code == KeyEvent.VK_ENTER) {
-                    if(gp.ui.commandNum == 0) {
-                        System.out.println("Haz algunas cosas especificas de...");
-                        gp.gameState = gp.playState;
-                        gp.reloj.reiniciarTiempo();
-                        gp.ui.titleScreenState = 1;
-                        gp.playMusic(0);
-                    }
-                    if(gp.ui.commandNum == 1) {
-                        System.out.println("Haz algunas cosas especificas de...");
-                        gp.gameState = gp.playState;
-                        gp.playMusic(0);
-                    }
-                    if(gp.ui.commandNum == 2) {
-                        System.out.println("Haz algunas cosas especificas de...");
-                        gp.gameState = gp.playState;
-                        gp.playMusic(0);
-                    }
-                    if(gp.ui.commandNum == 3) {
-                        gp.gameState = gp.titleState;
-
-                    }
-                }
-            }
         }
-
-
     }
-    public void playState(int code) {
 
+    public void playState(int code) {
+        // Player 1 - WASD
         if(code == KeyEvent.VK_W) {
             upPressed = true;
         }
         if(code == KeyEvent.VK_S) {
-            downPressed = true;
+            downPressed = true; // No se usa pero lo mantenemos
         }
         if(code == KeyEvent.VK_A) {
             leftPressed = true;
@@ -121,68 +102,81 @@ public class KeyHandler implements KeyListener{
         if(code == KeyEvent.VK_D) {
             rightPressed = true;
         }
+
+        // Player 2 - Flechas
+        if(code == KeyEvent.VK_UP) {
+            arrowUpPressed = true;
+        }
+        if(code == KeyEvent.VK_DOWN) {
+            arrowDownPressed = true; // No se usa pero lo mantenemos
+        }
+        if(code == KeyEvent.VK_LEFT) {
+            arrowLeftPressed = true;
+        }
+        if(code == KeyEvent.VK_RIGHT) {
+            arrowRightPressed = true;
+        }
+
+        // Controles generales
         if(code == KeyEvent.VK_C) {
             gp.gameState = gp.characterState;
         }
         if(code == KeyEvent.VK_ENTER) {
             enterPressed = true;
         }
-
+        if(code == KeyEvent.VK_ESCAPE) {
+            gp.gameState = gp.pauseState;
+        }
         if(code == KeyEvent.VK_T) {
-            if (checkDrawTime == false) {
-                checkDrawTime = true;
-            }
-            else if(checkDrawTime == true){
-                checkDrawTime = false;
-            }
+            checkDrawTime = !checkDrawTime;
         }
     }
+
     public void characterState(int code) {
         if(code == KeyEvent.VK_C) {
             gp.gameState = gp.playState;
         }
         if(gp.ui.slotRow != 0){
-            if(code == KeyEvent.VK_W) {
+            if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
                 gp.ui.slotRow--;
                 gp.playSE(5);
             }
         }
         if(gp.ui.slotCol != 0){
-            if(code == KeyEvent.VK_A) {
+            if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
                 gp.ui.slotCol--;
                 gp.playSE(5);
             }
         }
         if(gp.ui.slotRow != 3){
-            if(code == KeyEvent.VK_S) {
+            if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
                 gp.ui.slotRow++;
                 gp.playSE(5);
             }
         }
         if(gp.ui.slotCol != 4){
-            if(code == KeyEvent.VK_D) {
+            if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
                 gp.ui.slotCol++;
                 gp.playSE(5);
             }
         }
     }
-    public void dialogueState(int code) {
 
+    public void dialogueState(int code) {
         if(code == KeyEvent.VK_ENTER) {
             gp.gameState = gp.playState;
         }
     }
 
     public void gameOverState(int code){
-
-        if(code == KeyEvent.VK_W){
+        if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP){
             gp.ui.commandNum--;
             if(gp.ui.commandNum < 0) {
                 gp.ui.commandNum = 1;
             }
             gp.playSE(5);
         }
-        if(code == KeyEvent.VK_S){
+        if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN){
             gp.ui.commandNum++;
             if(gp.ui.commandNum > 1) {
                 gp.ui.commandNum = 0;
@@ -191,19 +185,19 @@ public class KeyHandler implements KeyListener{
         }
         if(code == KeyEvent.VK_ENTER){
             if(gp.ui.commandNum == 0) {
-                gp.reloj.reiniciarTiempo();
                 gp.gameState = gp.playState;
+                gp.setupGame();
+            } else {
                 System.exit(0);
             }
         }
     }
 
-
     @Override
     public void keyReleased(KeyEvent e) {
-
         int code = e.getKeyCode();
 
+        // Player 1 - WASD
         if(code == KeyEvent.VK_W) {
             upPressed = false;
         }
@@ -215,6 +209,20 @@ public class KeyHandler implements KeyListener{
         }
         if(code == KeyEvent.VK_D) {
             rightPressed = false;
+        }
+
+        // Player 2 - Flechas
+        if(code == KeyEvent.VK_UP) {
+            arrowUpPressed = false;
+        }
+        if(code == KeyEvent.VK_DOWN) {
+            arrowDownPressed = false;
+        }
+        if(code == KeyEvent.VK_LEFT) {
+            arrowLeftPressed = false;
+        }
+        if(code == KeyEvent.VK_RIGHT) {
+            arrowRightPressed = false;
         }
     }
 }
